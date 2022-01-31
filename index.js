d9be576d1202e96d00ed876faed37578cce7c8cd
Klemense <klemenseloh@gmail.com>
2022-01-31 17:22:11 -0500

    commit tree d5c8aa464d9cde4b643d20126057fc1f718aff89

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(target= "roller-skate"){
+    return (`This Saturday, I want to ${target}!`)
+}
+
+function mondayWork(target = "go to the office"){
+    return (`This Monday, I will ${target}.`);
+}
+
+function wrapAdjective(target){
+    return function(singleParam="special"){
+        return (`You are ${target}${singleParam}${target}!`);
+    }
+}
